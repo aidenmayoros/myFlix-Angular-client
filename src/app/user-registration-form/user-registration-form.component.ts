3bd7b36047a534317c94e341d2e0f08172596613
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-10-11 21:46:26 -0700

    Sign up modal working

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
+//Use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
+//This import brings in the API calls created in Task 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
+//Import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -25,17 +22,19 @@ export class UserRegistrationFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // This is the function responsible for sending the form inputs to the backend
+  //This function is responsible for sending the form inputs to the backend
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe(
-      () => {
-        // Logic for a successful user registration goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
-        this.snackBar.open('user registerd successfully', 'OK', {
+      (result) => {
+        //Logic for a successful user registration goes here
+        this.dialogRef.close(); //This will close the modal on success.
+        console.log(result);
+        this.snackBar.open('result', 'OK', {
           duration: 2000,
         });
       },
       (result) => {
+        console.log(result);
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
