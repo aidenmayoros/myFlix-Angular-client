09b26830df4eafb2fa85a63c6247913fd49bfd32
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-10-13 18:12:22 -0700

    Register modal working with back end

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -29,14 +29,14 @@ export class UserRegistrationFormComponent implements OnInit {
         //Logic for a successful user registration goes here
         this.dialogRef.close(); //This will close the modal on success.
         console.log(result);
-        this.snackBar.open('result', 'OK', {
-          duration: 2000,
+        this.snackBar.open('User successfully registered', 'OK', {
+          duration: 5000,
         });
       },
       (result) => {
         console.log(result);
         this.snackBar.open(result, 'OK', {
-          duration: 2000,
+          duration: 5000,
         });
       },
     );
